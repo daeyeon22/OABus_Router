84fd8ea25bc288419cb89a3bef6c5f4a5e255442
daeyeon22 <daeyeon2@unist.ac.kr>
2018-08-23 15:58:49 +0900

    rip.cpp added

---
+++ code/rip.cpp
@@ -0,0 +1,22 @@
+#include "route.h"
+
+
+
+
+int OABusRouter::Router::get_congested_bus(int busid)
+{
+
+    return 0;
+}
+
+void OABusRouter::Router::rip_up(int busid)
+{
+
+}
+
+bool OABusRouter::Router::reroute(int busid)
+{
+
+
+    return true;
+}
